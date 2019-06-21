9e66adea98944d7cf0499d6edc38fb64f92e433a
fabien.lebronnec <fabien.lebronnec@compta.com>
2019-06-21 11:32:22 +0200

    Tests

--- src/test/java/com/notreprojet/back/command/AddCommandTest.java
+++ src/test/java/com/notreprojet/back/command/AddCommandTest.java
@@ -1,6 +1,7 @@
 package com.notreprojet.back.command;
 
 import com.notreprojet.back.calculus.Calculator;
+import com.notreprojet.back.parsing.Operators;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -9,6 +10,24 @@ import org.mockito.Mockito;
  * Test class for @{link AddCommand}.
  */
 public class AddCommandTest {
+
+	@Test
+	public void getCalculator() {
+		Calculator calculator = Mockito.mock(Calculator.class);
+		Assert.assertEquals(calculator, new AddCommand(calculator, 6).getCalculator());
+	}
+
+	@Test
+	public void getMember() {
+		Calculator calculator = Mockito.mock(Calculator.class);
+		Assert.assertEquals(6F, new AddCommand(calculator, 6).getMember(), 0);
+	}
+
+	@Test
+	public void getOperator() {
+		Calculator calculator = Mockito.mock(Calculator.class);
+		Assert.assertEquals(Operators.PLUS, new AddCommand(calculator, 6).getOperator());
+	}
 
 	@Test
 	public void execute() {
